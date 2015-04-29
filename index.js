2ca7a928ea18c081b1f13d45e0011bddad507975
christianalfoni <christianalfoni@gmail.com>
2015-04-29 03:56:37 +0200

    Added count and changed to options map

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-module.exports = function (callback, wait) {
+module.exports = function(callback, options) {
+  options = options || {};
   var calls = [];
   var dispatch = null;
-  return function () {
-    if (!dispatch) {
-      dispatch = setTimeout(function () {
-        callback.apply(null, calls);
-        calls = [];
-        dispatch = null;
-      }, wait || 0);
+  var count = 0;
+  var execute = function() {
+    callback.apply(null, calls);
+    calls = [];
+    dispatch = null;
+    count = 0;
+  };
+  return function() {
+    count++;
+    if ((!options.count && !dispatch) || count === options.count) {
+      dispatch = options.wait || !options.count ? setTimeout(execute, options.wait || 0) : execute();
     }
     for (var x = 0; x < arguments.length; x++) {
       calls[x] = calls[x] ? calls[x].concat(arguments[x]) : [arguments[x]];
--- test.js
+++ test.js
@@ -10,6 +10,27 @@ exports['should batch calls'] = function(test){
     func('3');
 };
 
+exports['should batch calls multiple times'] = function(test){
+    var count = 0;
+    var func = batchCalls(function (ids) {
+      if (count === 0) {
+        test.deepEqual(ids, ['1', '2', '3']);
+        count++;
+      } else {
+        test.deepEqual(ids, ['4', '5', '6']);
+        test.done();
+      }
+    });
+    func('1');
+    func('2');
+    func('3');
+    setTimeout(function () {
+      func('4');
+      func('5');
+      func('6');
+    }, 0);
+};
+
 exports['should batch calls with multiple arguments'] = function(test){
     var func = batchCalls(function (argsA, argsB, argsC) {
       test.deepEqual(argsA, ['1', '2', '1']);
@@ -27,6 +48,39 @@ exports['should wait set milliseconds'] = function(test){
     var func = batchCalls(function () {
       test.ok(Date.now() - start >= 100);
       test.done();
-    }, 100);
+    }, {
+      wait: 100
+    });
     func();
+};
+
+exports['should wait x calls and synchronously trigger'] = function(test){
+    var func = batchCalls(function () {
+      test.done();
+    }, {
+      count: 3
+    });
+    func();
+    func();
+    func();
+
+    // Fail test if async
+    setTimeout(function () {
+      test.ok(false);
+    }, 0);
+};
+
+exports['should wait x calls and wait x milliseconds before trigger'] = function(test){
+    var start = Date.now();
+    var func = batchCalls(function () {
+      test.ok(Date.now() - start >= 100);
+      test.done();
+    }, {
+      count: 3,
+      wait: 100
+    });
+    func();
+    func();
+    func();
+
 };
