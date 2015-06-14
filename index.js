f5cc3d5aca8cb3d0e47938a910f26635260eb1a7
christianalfoni <christianalfoni@gmail.com>
2015-06-14 17:48:32 +0200

    callback runs in context of function returned

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function(callback, options) {
   var dispatch = null;
   var count = 0;
   var execute = function() {
-    callback.apply(null, calls);
+    callback.apply(this, calls);
     calls = [];
     dispatch = null;
     count = 0;
@@ -12,7 +12,7 @@ module.exports = function(callback, options) {
   return function() {
     count++;
     if ((!options.count && !dispatch) || count === options.count) {
-      dispatch = options.wait || !options.count ? setTimeout(execute, options.wait || 0) : execute();
+      dispatch = options.wait || !options.count ? setTimeout(execute.bind(this), options.wait || 0) : execute();
     }
     for (var x = 0; x < arguments.length; x++) {
       calls[x] = calls[x] ? calls[x].concat(arguments[x]) : [arguments[x]];
